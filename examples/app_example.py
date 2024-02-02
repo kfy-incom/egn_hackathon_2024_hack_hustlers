843d368ab2cb50e1ea436a6cd64b428fa99f3a69
eijo@bunker-holding.com <eijo@bunker-holding.com>
2024-02-02 11:02:07 +0100

    adding examples

---
+++ examples/app_example.py
@@ -0,0 +1,115 @@
+import json
+
+import numpy as np
+import openai
+import streamlit as st
+from annoy import AnnoyIndex
+from streamlit_chat import message
+
+
+def _submit():
+    st.session_state['question'] = st.session_state.input_field
+    st.session_state.input_field = ''
+
+
+def app():
+    # Build Streamlit App
+    # Setup azure openai
+    openai.api_type = "azure"
+    openai.api_base = "https://cs-openai-us-jml.openai.azure.com/"
+    openai.api_version = "2023-05-15"
+    openai.api_key = "ff9de7d753b5443b9846bfb3de8e6edb"
+
+    st.title("Team Area 51 : Alien Nerds")
+    st.image("aliennerds.jpg", width = 650)
+    st.title("Kapacity LLM Hackathon")
+    f = 1536
+    EMBEDDING_DTYPE = np.float32
+    st.write("This is group Area 51 submission for the LLM Hackathon.")
+    u = AnnoyIndex(f, 'angular')
+    u.load('queen_speeches.ann')
+    a = AnnoyIndex(f, 'angular')
+    a.load('queen_speeches_summaries.ann')
+    with open('processed_speeches.json') as fp:
+        text_dict = json.load(fp)
+    with open('processed_speeches_summaries.json') as fp:
+        text_summaries_dict = json.load(fp)
+    if 'question' not in st.session_state:
+        st.session_state['question'] = ''
+
+    placeholder = st.empty()
+
+    if 'conversation' not in st.session_state:
+        st.session_state['conversation'] = [{"message": "Hi! How may I help you today?", "is_user": False}]
+        with placeholder.container():
+            message(st.session_state['conversation'][0]["message"], is_user=False, key="start")
+
+    st.text_input("you:", key="input_field", on_change=_submit)
+
+    count = 0
+    if st.session_state['question']:
+        with placeholder.container():
+            for message_ in st.session_state['conversation']:
+                message(message_["message"], is_user=message_["is_user"], key=count)
+                count += 1
+
+        with st.spinner(f"Generate answer for question: {st.session_state['question']}"):
+            embedding = openai.Embedding.create(input=st.session_state['question'],
+                                                engine='text-embedding-ada-002')['data'][0]['embedding']
+            embedding = np.array(embedding, dtype=EMBEDDING_DTYPE)
+            vectors = u.get_nns_by_vector(embedding, 5, search_k=-1, include_distances=True)
+            vectors_summaries = a.get_nns_by_vector(embedding, 10, search_k=-1, include_distances=True)
+            print(vectors)
+            texts = [text_dict[str(i)] for i in vectors[0]]
+            summaries = [text_summaries_dict[str(i)] for i in vectors_summaries[0]]
+                     #You are answering questions to the best of your ability. 
+            selected_conversation_hist = [
+                {"role": "system",
+                 "content": f"""
+                 Look for named entities in the text and the context it is in and the year.
+                 when possible keep the answer short.
+                    You are a polite and helpful assistant having a conversation with a human. 
+                     You are not trying to be funny or clever. You are trying to be helpful. 
+                     You are not trying to show off.
+                     You will be answering questions specifically about the Danish queens new years speeches. 
+                     You will be asked for themes, events within specific years, count of events and what she has talked
+                     about. Please answer as detailed as you can within the context of the question.
+                     If asked about the number of individuals remember to count the names of the individuals mentioned in a specific context.
+                     Have family relations in mind.
+                     If you dont have a specific answer then give the closest answer possible keep the question context in mind.
+                     If you asked about context of something the refer back to what events are related to said something.
+                     """},
+            ]
+            print(texts)
+            modified_question = "Brugeren spurgte: \n" + \
+                                st.session_state['question'] + \
+                                '\n Du har nu følgende fra dronningens nytårstaler fra 2001-2022 at svare ud fra: \n' + \
+                                '\n '.join(texts) + \
+                                '\n Og disse følgende tekster fra opsummeringer: \n' + \
+                                '\n '.join(summaries) + \
+                                '\n Besvar spørgsmålet.'
+            # Add question to conversation
+            messages = selected_conversation_hist + [{"role": "user", "content": modified_question}]
+
+            raw_answer = openai.ChatCompletion.create(
+                engine="gpt-35-turbo",
+                messages=messages
+            )
+            answer = raw_answer["choices"][0]["message"]["content"]
+
+        # Save new question and answer in the session state
+        st.session_state['conversation'].append({"message": st.session_state['question'], "is_user": True})
+        st.session_state['conversation'].append({"message": answer, "is_user": False})
+
+        # Show new question and answer
+        with placeholder.container():
+            for message_ in st.session_state['conversation']:
+                message(message_["message"], is_user=message_["is_user"], key=count)
+                count += 1
+
+        # Reset question state
+        st.session_state['question'] = ''
+
+
+if __name__ == "__main__":
+    app()
---
+++ examples/db_example.py
@@ -0,0 +1,66 @@
+from annoy import AnnoyIndex
+import openai
+import numpy as np
+import json
+EMBEDDING_DTYPE = np.float32
+import os
+openai.api_type = "azure"
+openai.api_base = "https://cs-openai-us-jml.openai.azure.com/"
+openai.api_version = "2023-05-15"
+openai.api_key = "ff9de7d753b5443b9846bfb3de8e6edb"
+f = 1536
+t = AnnoyIndex(f, 'angular')
+z = AnnoyIndex(f, 'angular')
+idx = 0
+idx_z = 0
+text_dict = {}
+text_dict_summary = {}
+for i in range(2001, 2023):
+    with open(f'/app/queens_speeches/speeches/{i}.txt') as f:
+        total_text = f"År {i}"
+        for line_idx, line in enumerate(f.readlines()):
+            total_text += '\n' + line
+            line = line.strip()
+            if len(line)>0:
+                line = f'År: {i}\n Linjenummer: {line_idx} \n' +  line
+                embedding = openai.Embedding.create(input=line, engine='text-embedding-ada-002')['data'][0]['embedding']
+                embedding = np.array(embedding, dtype=EMBEDDING_DTYPE)
+                t.add_item(idx, embedding)
+                text_dict[idx] = line
+                idx += 1
+    selected_conversation_hist = [
+        {"role": "system",
+         "content": f"""
+            Your job is to summarize the Danish Queens new years speeches.
+            Please include information such as but not limited to 
+            themes, people, countries, facts, events and minorities.
+            Please do so in Danish.
+             """},
+    ]
+    modified_question = "Opsummer følgende tale: \n" + \
+                        total_text
+    # Add question to conversation
+    messages = selected_conversation_hist + [{"role": "user", "content": modified_question}]
+
+    raw_answer = openai.ChatCompletion.create(
+        engine="gpt-35-turbo",
+        messages=messages
+    )
+    answer = raw_answer["choices"][0]["message"]["content"]
+    for summary_idx, summary_text in enumerate(answer.split('.')):
+        summary_text = f'År: {i}\n Linjenummer: {summary_idx} \n' +  summary_text
+        embedding = openai.Embedding.create(input=summary_text, engine='text-embedding-ada-002')['data'][0]['embedding']
+        embedding = np.array(embedding, dtype=EMBEDDING_DTYPE)
+        z.add_item(idx_z, embedding)
+        text_dict_summary[idx_z] = summary_text
+        idx_z += 1
+    print(i)
+t.build(10) # 10 trees
+t.save('queen_speeches.ann')
+z.build(10)
+z.save('queen_speeches_summaries.ann')
+
+with open('processed_speeches.json', 'w') as fp:
+    json.dump(text_dict, fp)
+with open('processed_speeches_summaries.json', 'w') as fp:
+    json.dump(text_dict_summary, fp)
